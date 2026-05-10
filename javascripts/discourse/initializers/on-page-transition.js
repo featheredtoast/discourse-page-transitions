5bf897b828b7437ec9a6060c627f6bce1b65ffa9
Jeff Wong <awole20@gmail.com>
2026-05-09 17:22:33 -0700

    experiment with behavior transformer directly on discovery

--- javascripts/discourse/initializers/on-page-transition.js
+++ javascripts/discourse/initializers/on-page-transition.js
@@ -1,19 +1,26 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
 export default {
   name: "page-transitions",
 
   initialize(container) {
-    const router = container.lookup("service:router");
-    router.on('routeWillChange', (transition) => {
-      console.log("route will change");
-      console.log(transition?.from?.name);
-      console.log(transition?.to?.name);
-      // view transition should be waiting on the router to settle
-      // route will change is within a router, so this should be async?
-      //set scroll based on y scroll for old transition element
-      document.documentElement.style.setProperty('--vt-scroll-y-old', `${-document.documentElement.scrollTop}px`);
-      return document.startViewTransition(() => {
-        return transition.promise;
-      }).ready;
+    withPluginApi((api) => {
+      api.registerBehaviorTransformer(
+        "discovery:before-model",
+        ({ next, context }) => {
+          const transition = context.transition;
+          console.log("before model transition discovery");
+          document.documentElement.style.setProperty('--vt-scroll-y-old', `${-document.documentElement.scrollTop}px`);
+          document.startViewTransition(() => transition.promise);
+          next();
+        }
+      );
+
+      const viewTransition = {
+        async beforeModel(transition) {
+          console.log("before model");
+        },
+      };
     });
   }
 }
